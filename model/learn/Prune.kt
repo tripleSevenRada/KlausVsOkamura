7c6d3b106b6567b454618c3d0fb4e246029b0aa3
radim <radvecerek@yahoo.co.uk>
2018-10-22 17:04:46 +0200

    simplified prune syntax

--- model/learn/Prune.kt
+++ model/learn/Prune.kt
@@ -5,12 +5,6 @@ class PrunePunctuation{
 	private val interPunctChars: Set<Char> = setOf('.','?','!',',',';',':','_','-','(',')','{','}','[',']','“','„','"','/')
 	
 	fun prune(rawList: List<String>): List<String>{
-		val pruned = mutableListOf<String>()
-		val returned: List<String> = pruned
-		rawList.forEach{
-			if(it.length == 1 && interPunctChars.contains(it.get(0))) return@forEach
-			pruned.add(it)
-		}
-		return returned
+		return rawList.filter { !(it.length == 1 && interPunctChars.contains(it.get(0))) }
 	}
 }
